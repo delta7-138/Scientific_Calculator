ac43ab6528a52468e2a1f6dfff8ac48dd0897625
delta7-138 <krishna.sharma@iiitb.org>
2023-03-14 22:31:40 +0530

    [Updation of config] changing pom.xml to build with main manifest

---
+++ src/main/java/App.java
@@ -0,0 +1,28 @@
+/**
+ * Hello world!
+ *
+ */
+
+import java.util.Scanner; 
+
+public class App 
+{
+    public static int fact(int n)
+    {
+        if(n == 0)
+        {
+            return 1; 
+        }
+
+        return n * fact(n-1); 
+    }
+
+    public static void main( String[] args )
+    {   
+        Scanner scn = new Scanner(System.in); 
+        System.out.println("Enter a number : "); 
+        int n = scn.nextInt(); 
+
+        System.out.println(fact(n)); 
+    }
+}
